d04e3746b7621488ee0def45345997d7d3d0f258
Ed Penington <edpenington@Eds-MacBook-Air.local>
2025-02-17 16:22:42 +0000

    expand eip trajectories to all components and move parameters out of function argument

--- src/eip_pathway_model.R
+++ src/eip_pathway_model.R
@@ -9,117 +9,283 @@ library(simmer)
 library(simmer.plot)
 library(dplyr)
 
-create_eip_trajectories <- function(
-    env, 
-    params = list(
-      # Default sample parameters - real functions to be devised and passed as arguments
-      
-      # Care coordination parameters
-      T_EIP_DURATION = function() max(1, rnorm(1, 365*3, 90)),
-      T_CBT_DURATION = function() max(1, rnorm(1, 7*16, 7)),
-    
-    )
-) {
+# MODEL PARAMETERS -------------------------------------------------------
+
+# Duration parameters (in days)
+EIP_DURATION <- 365 * 3    # 3 years of EIP care
+CBT_DURATION <- 7 * 16     # 16 weekly sessions
+FI_DURATION <- 7 * 10      # 10 weekly sessions
+CARER_DURATION <- 7 * 8    # 8 weekly sessions
+IPS_DURATION <- 365        # 1 year of support
+PHYSICAL_DURATION <- 7     # 1 week for assessment and intervention
+
+# Time-to-event functions
+get_eip_duration <- function() EIP_DURATION
+get_cbt_duration <- function() CBT_DURATION  
+get_fi_duration <- function() FI_DURATION
+get_carer_duration <- function() CARER_DURATION
+get_ips_duration <- function() IPS_DURATION
+get_physical_duration <- function() PHYSICAL_DURATION
+
+# Acceptance probabilities (to be replaced with more sophisticated logic)
+CBT_ACCEPT_PROB <- 0.7     # 70% accept CBT when offered
+FI_ACCEPT_PROB <- 0.6      # 60% accept FI when offered
+CARER_ACCEPT_PROB <- 0.8   # 80% accept carer intervention
+IPS_ACCEPT_PROB <- 0.5     # 50% accept IPS
+PHYSICAL_ACCEPT_PROB <- 0.9 # 90% accept physical health intervention
+
+# HELPER FUNCTIONS ------------------------------------------------------
+
+# Function to create signal names for component outcomes
+create_signal_name <- function(component, id, outcome) {
+  paste0(component, "_", outcome, "_", id)
+}
+
+# DEFINE EIP TRAJECTORY ------------------------------------------------
+
+create_eip_trajectories <- function(env) {
   
   # Set up a global counter for unique IDs
   env %>% add_global("id_counter", 0)
   
-  # Trapping trajectory for recording receipt of components
-  trap_traj <- trajectory("trap") %>%
-    log_(function() paste0("Starting trap trajectory for user ", get_attribute(env, "id"))) %>%
+  # Monitoring trajectory
+  monitor_traj <- trajectory("monitor") %>%
+    log_(function() paste0("Starting monitoring trajectory for user ", get_attribute(env, "id"))) %>%
     set_attribute("clone_branch", 1) %>%
-    # Care coordinator discharge
+    # Initialize receipt tracking attributes
     set_attribute("cc_discharge", 0) %>%
-    trap(function() paste0("cc_discharge_", get_attribute(env, "id")),
-         trajectory() %>%
-           log_(function() paste0("Received signal cc_receipt for user ", get_attribute(env, "id"))) %>%
-           set_attribute("cc_discharge", 1)
-    ) %>%
-    # CBT receipt
     set_attribute("cbt_receipt", 0) %>%
-    trap(function() paste0("cbt_receipt_", get_attribute(env, "id")),
-         trajectory() %>%
-           log_(function() paste0("Received signal cbt_receipt for user ", get_attribute(env, "id"))) %>%
-           set_attribute("cbt_receipt", 1)
-    ) %>%
-    # Waits
-    wait() %>% wait() %>%
-    timeout(0.01)  
+    set_attribute("fi_receipt", 0) %>%
+    set_attribute("carer_receipt", 0) %>%
+    set_attribute("ips_receipt", 0) %>%
+    set_attribute("physical_receipt", 0) %>%
+    # Set up traps for acceptances
+    trap(function() create_signal_name("cc", get_attribute(env, "id"), "discharge"),
+         trajectory() %>% set_attribute("cc_discharge", 1)) %>%
+    trap(function() create_signal_name("cbt", get_attribute(env, "id"), "accept"),
+         trajectory() %>% set_attribute("cbt_receipt", 1)) %>%
+    trap(function() create_signal_name("fi", get_attribute(env, "id"), "accept"),
+         trajectory() %>% set_attribute("fi_receipt", 1)) %>%
+    trap(function() create_signal_name("carer", get_attribute(env, "id"), "accept"),
+         trajectory() %>% set_attribute("carer_receipt", 1)) %>%
+    trap(function() create_signal_name("ips", get_attribute(env, "id"), "accept"),
+         trajectory() %>% set_attribute("ips_receipt", 1)) %>%
+    trap(function() create_signal_name("physical", get_attribute(env, "id"), "accept"),
+         trajectory() %>% set_attribute("physical_receipt", 1)) %>%
+    
+    # Set up traps for refusals
+    trap(function() create_signal_name("cbt", get_attribute(env, "id"), "refuse"),
+         trajectory() %>% set_attribute("cbt_receipt", 99)) %>%
+    trap(function() create_signal_name("fi", get_attribute(env, "id"), "refuse"),
+         trajectory() %>% set_attribute("fi_receipt", 99)) %>%
+    trap(function() create_signal_name("carer", get_attribute(env, "id"), "refuse"),
+         trajectory() %>% set_attribute("carer_receipt", 99)) %>%
+    trap(function() create_signal_name("ips", get_attribute(env, "id"), "refuse"),
+         trajectory() %>% set_attribute("ips_receipt", 99)) %>%
+    trap(function() create_signal_name("physical", get_attribute(env, "id"), "refuse"),
+         trajectory() %>% set_attribute("physical_receipt", 99)) %>%
+    
+    # Wait for all signals
+    # Wait for signals
+    wait() %>% wait() %>% wait() %>% wait() %>% wait() %>% wait() %>%
+    timeout(0.01)
   
   # Care coordination trajectory
   carecoord_traj <- trajectory("eip_carecoord") %>%
-    log_(function() paste("Joining queue for care coordination for user", get_attribute(env, "id"))) %>%
     set_attribute("clone_branch", 2) %>%
     seize("care_coordinator", 1) %>%
-    timeout(params$T_EIP_DURATION) %>%
+    timeout(get_eip_duration) %>%
     release("care_coordinator", 1) %>%
-    log_(function() paste("Ending care coordination for user", get_attribute(env, "id"))) %>%
-    log_(function() paste("Sending signal that care coordination has ended for user ", get_attribute(env, "id"))) %>%
-    send(function() paste0("cc_discharge_", get_attribute(env, "id")))
-    
+    send(function() create_signal_name("cc", get_attribute(env, "id"), "discharge"))
   
-  # CBT trajectory
+  # CBT trajectory with acceptance branch
   cbt_traj <- trajectory("cbt") %>%
-    log_(function() paste0("Starting CBTp for user ", get_attribute(env, "id"))) %>%
     set_attribute("clone_branch", 3) %>%
-    seize("cbt_therapist", 1) %>%
-    timeout(params$T_CBT_DURATION) %>%
-    release("cbt_therapist", 1) %>%
-    log_(function() paste("CBT complete for user", get_attribute(env, "id"))) %>% 
-    log_(function() paste("Sending signal that CBT has been received for user ", get_attribute(env, "id"))) %>%
-    send(function() paste0("cbt_receipt_", get_attribute(env, "id"))) %>%
-    set_attribute("cbt_end", now(env))
-
+    branch(
+      function() runif(1) < CBT_ACCEPT_PROB,
+      continue = TRUE,
+      trajectory() %>%
+        seize("cbt_therapist", 1) %>%
+        timeout(get_cbt_duration) %>%
+        release("cbt_therapist", 1) %>%
+        send(function() create_signal_name("cbt", get_attribute(env, "id"), "accept")),
+      trajectory() %>%
+        send(function() create_signal_name("cbt", get_attribute(env, "id"), "refuse"))
+    )
+  
+  # Family Intervention trajectory with acceptance branch
+  fi_traj <- trajectory("fi") %>%
+    set_attribute("clone_branch", 4) %>%
+    branch(
+      function() runif(1) < FI_ACCEPT_PROB,
+      continue = TRUE,
+      trajectory() %>%
+        seize("fi_therapist", 1) %>%
+        timeout(get_fi_duration) %>%
+        release("fi_therapist", 1) %>%
+        send(function() create_signal_name("fi", get_attribute(env, "id"), "accept")),
+      trajectory() %>%
+        send(function() create_signal_name("fi", get_attribute(env, "id"), "refuse"))
+    )
+  
+  # Carer intervention trajectory with acceptance branch
+  carer_traj <- trajectory("carer") %>%
+    set_attribute("clone_branch", 5) %>%
+    branch(
+      function() runif(1) < CARER_ACCEPT_PROB,
+      continue = TRUE,
+      trajectory() %>%
+        seize("carer_worker", 1) %>%
+        timeout(get_carer_duration) %>%
+        release("carer_worker", 1) %>%
+        send(function() create_signal_name("carer", get_attribute(env, "id"), "accept")),
+      trajectory() %>%
+        send(function() create_signal_name("carer", get_attribute(env, "id"), "refuse"))
+    )
+  
+  # Employment support trajectory with acceptance branch
+  ips_traj <- trajectory("ips") %>%
+    set_attribute("clone_branch", 6) %>%
+    branch(
+      function() runif(1) < IPS_ACCEPT_PROB,
+      continue = TRUE,
+      trajectory() %>%
+        seize("employment_specialist", 1) %>%
+        timeout(get_ips_duration) %>%
+        release("employment_specialist", 1) %>%
+        send(function() create_signal_name("ips", get_attribute(env, "id"), "accept")),
+      trajectory() %>%
+        send(function() create_signal_name("ips", get_attribute(env, "id"), "refuse"))
+    )
+  
+  # Physical health trajectory with acceptance branch
+  physical_traj <- trajectory("physical") %>%
+    set_attribute("clone_branch", 7) %>%
+    branch(
+      function() runif(1) < PHYSICAL_ACCEPT_PROB,
+      continue = TRUE,
+      trajectory() %>%
+        seize("physical_health_specialist", 1) %>%
+        timeout(get_physical_duration) %>%
+        release("physical_health_specialist", 1) %>%
+        send(function() create_signal_name("physical", get_attribute(env, "id"), "accept")),
+      trajectory() %>%
+        send(function() create_signal_name("physical", get_attribute(env, "id"), "refuse"))
+    )
   
   # Main EIP trajectory
-  eip_pathway_traj <- trajectory("main") %>%
-    log_("Starting EIP pathway") %>%
-    # Add unique ID at start
+  main_traj <- trajectory("main") %>%
+    # Add unique ID and initialize attributes
     set_global("id_counter", 1, mod="+") %>%
-    set_attribute("id", function() get_global(env, "id_counter")) %>% 
+    set_attribute("id", function() get_global(env, "id_counter")) %>%
     set_attribute("clone_branch", 0) %>%
-    
+    set_attribute("entry_time", function() now(env)) %>%
     
     # Clone and distribute to EIP components
     clone(
-      n = 3,
+      n = 7,
       carecoord_traj,
       cbt_traj,
-      trap_traj
-    ) %>%
-    log_(function() paste0("Trajectory cloned for user ", get_attribute(env, "id"))) %>%
+      fi_traj,
+      carer_traj,
+      ips_traj,
+      physical_traj,
+      monitor_traj
+    )   %>%
     synchronize(wait = TRUE, mon_all = TRUE) %>%
-    log_(function() paste0("Trajectories synchronized for user ", get_attribute(env, "id"))) %>%
-    log_(function() paste0("Attributes for user ", get_attribute(env, "id"), 
-                           ": cc_discharge - ", get_attribute(env, "cc_discharge"),
-                           ", cbt_receipt - ", get_attribute(env, "cbt_receipt"),
-                           ", clone_branch - ", get_attribute(env, "clone_branch")))
-  
-  return(eip_pathway_traj)
+    log_(function() paste0(
+      "Post-synchronization for user ", get_attribute(env, "id"),
+      " branch ", get_attribute(env, "clone_branch"), 
+      " at time ", now(env),
+      "\n  cc_discharge: ", get_attribute(env, "cc_discharge"),
+      "\n  cbt_receipt: ", get_attribute(env, "cbt_receipt"),
+      "\n  fi_receipt: ", get_attribute(env, "fi_receipt"),
+      "\n  carer_receipt: ", get_attribute(env, "carer_receipt"),
+      "\n  ips_receipt: ", get_attribute(env, "ips_receipt"),
+      "\n  physical_receipt: ", get_attribute(env, "physical_receipt")
+    ))
+  return(main_traj)
 }
 
+env <- simmer("env")
 
-# Example usage
-if (FALSE) {
+eip_traj <- create_eip_trajectories(env)
+plot(eip_traj)
+
+# RUN SIMULATION -------------------------------------------------------
+
+run_eip_simulation <- function(
+    yearly_caseload,   # Expected number of new referrals per year
+    resources = list(  # Resource levels (FTE staff)
+      care_coordinator = 15,
+      cbt_therapist = 5,
+      fi_therapist = 4,
+      carer_worker = 5,
+      employment_specialist = 5,
+      physical_health_specialist = 1
+    ),
+    sim_duration = 365*10  # Simulation duration in days
+) {
+  
+  # Calculate arrival rate (in days) from yearly caseload
+  arrival_rate <- 365/yearly_caseload
+  
+  # Create simulation environment
   env <- simmer("EIP_pathway")
   
   # Add resources
-  env %>%
-    add_resource("care_coordinator", 1) %>%
-    add_resource("cbt_therapist", 1) 
+  for (resource_name in names(resources)) {
+    env %>% add_resource(resource_name, resources[[resource_name]])
+  }
   
-  # Get trajectory
+  # Create trajectory
   eip_traj <- create_eip_trajectories(env)
-  plot(eip_traj, verbose = TRUE)
   
-  # Add generator
-  env %>%
-    add_generator("service_user", eip_traj, at(0), mon = 2) %>%  # Generate one entity at time 0
-    run(until = 10000)
+  # Generate arrival times
+  arrival_times <- cumsum(rexp(ceiling(sim_duration/arrival_rate * 1.5), 1/arrival_rate))
+  arrival_times <- arrival_times[arrival_times <= sim_duration]
   
-  # Get monitoring data
-  attributes <- get_mon_attributes(env)
-  arrivals <- get_mon_arrivals(env, per_resource = TRUE, ongoing = TRUE)
-  resources <- get_mon_resources(env)
-  print(attributes)
+  # Add generator and run simulation
+  env %>% 
+    add_generator("service_user", eip_traj, at(arrival_times), mon = 2) %>%
+    run(until = sim_duration)
+  
+  # Return results
+  list(
+    attributes = get_mon_attributes(env),
+    arrivals = get_mon_arrivals(env, per_resource = TRUE, ongoing = TRUE),
+    resources = get_mon_resources(env)
+  )
+}
+
+# EXAMPLE USAGE -------------------------------------------------------
+
+if (TRUE) {
+  # Run simulation with 50 service users per year
+  results <- run_eip_simulation(
+    yearly_caseload = 50,
+    resources = list(
+      care_coordinator = 3,
+      cbt_therapist = 2,
+      fi_therapist = 1,
+      carer_worker = 1,
+      employment_specialist = 1, 
+      physical_health_specialist = 1
+    )
+  )
+  
+  # Analyze receipt rates at end of simulation
+  receipt_rates <- results$attributes %>%
+    filter(name != "") %>%
+    mutate(discharged = if_else(key=="cc_discharge" & value == 1, 1, 0)) %>%
+    group_by(name) %>%
+    filter(max(discharged) == 1) %>% ungroup() %>%
+    group_by(name, key) %>%
+    filter(time == max(time)) %>% ungroup() %>%
+    select(name, key, value) %>%
+    filter(grepl("_receipt$", key))   %>%
+    group_by(key) %>%
+    summarize(rate = mean(value))
+  
+  print(receipt_rates)
 }
